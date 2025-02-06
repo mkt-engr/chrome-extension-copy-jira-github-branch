df93fd27464a31b2d8b12037e28c3807684470b6
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2025-02-06 22:10:34 +0900

    開いているタブのURLを取得

---
+++ pages/content/src/createCopyBranchNameButton.ts
@@ -0,0 +1,9 @@
+const isJiraPage = () => {
+  console.log('isJiraPage');
+  console.log(window.location.href);
+};
+
+export const createCopyBranchNameButton = () => {
+  isJiraPage();
+  console.log('createCopyBranchNameButton');
+};
--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -1,6 +1,9 @@
 import { sampleFunction } from '@src/sampleFunction';
+import { createCopyBranchNameButton } from '@src/createCopyBranchNameButton';
 
 console.log('content script loaded');
 
 // Shows how to call a function defined in another module
-sampleFunction();
+// sampleFunction();
+
+createCopyBranchNameButton();
